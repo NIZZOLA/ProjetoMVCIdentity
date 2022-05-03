13a19a7af3875353d90d0b63119df01d96f21793
Marcio Rogerio Nizzola <marcionizzola@gmail.com>
2022-05-02 22:33:56 -0300

    alterações para realizar scaffolding sem campos internos

--- ProjetoMVC/Models/BaseModel.cs
+++ ProjetoMVC/Models/BaseModel.cs
@@ -7,11 +7,17 @@ namespace ProjetoMVC.Models
         [Key]
         public Guid? Id { get; set; }
 
+        [ScaffoldColumn(false)]
         public DateTime? DataCriacao { get; set; }
+        [ScaffoldColumn(false)]
         public Guid? UsuarioIdCriacao { get; set; }
+        [ScaffoldColumn(false)]
         public DateTime? DataAlteracao { get; set; }
+        [ScaffoldColumn(false)]
         public Guid? UsuarioIdAlteracao { get; set; }
+        [ScaffoldColumn(false)]
         public DateTime? DataExclusao { get; set; }
+        [ScaffoldColumn(false)]
         public Guid? UsuarioIdExclusao { get; set; }
     }
 }
---
+++ ProjetoMVC/Models/ProjectConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoMVC.Models
+{
+    public static class ProjectConstants
+    {
+        public const string ErrorMessageFieldSize =
+            "O campo {0} tem tamanho máximo de {1} caracteres";
+    }
+}
